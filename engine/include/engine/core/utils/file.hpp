7e503f3eec04e5c0e5abcc00786f8d3b87b54187
apachano <pachano.austin@gmail.com>
2019-01-04 14:53:02 -0800

    Created initial header file to store vectors of loaded content

---
+++ engine/include/engine/core/utils/file.hpp
@@ -0,0 +1,22 @@
+#include <experimental/filesystem>
+#include <engine/core/Core.hpp>
+
+namespace phx{
+    class Content{
+        Content();
+        ~Content();
+
+        void load(); //Scan through content folders for any content
+
+        //Getters and setters
+
+        std::string getGame(std::string gamename);
+        std::string getMod(std::string modname);
+
+        private:
+            //Vectors to hold loaded content packs
+            std::vector <std::string>m_games;
+            std::vector <std::string>m_mods;
+            std::vector <std::string>m_texturePacks;
+    }
+}
