2e4e2d3ea325c6a4ba6c812d4307df8661e46a41
apachano <pachano.austin@gmail.com>
2019-01-04 15:03:42 -0800

    Created initial cpp file

---
+++ engine/src/core/utils/file.cpp
@@ -0,0 +1,23 @@
+#include <engine/core/utils/file.hpp>
+
+using namespace phx;
+
+void load(){
+    //Create content directories if they don't exist
+    if(! fs::exists("games")) fs::create_directory("games");
+    if(! fs::exists("mods")) fs::create_directory("mods");
+    if(! fs::exists("texturepacks")) fs::create_directory("texturepacks");
+
+    for (const auto & entry : fs::directory_iterator(path))
+        if(fs::exists(entry.path().string() + "/info.txt" ))
+            m_games.push_back("games/"+entry.path());
+};
+
+//Getters and setters
+
+std::string getGame(std::string gamename){
+
+};
+std::string getMod(std::string modname){
+
+};
